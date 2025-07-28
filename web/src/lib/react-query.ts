ba0aefd97ec3ab4421868f53a8b287f9f1754967
JeffreyJPZ <jeffreyjpizhang@gmail.com>
2025-07-28 15:54:17 -0700

    chore: add react query

---
+++ web/src/lib/react-query.ts
@@ -0,0 +1,29 @@
+import { QueryClient, QueryClientConfig, UseMutationOptions } from "@tanstack/react-query";
+
+const queryClientConfig: QueryClientConfig = {
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            retry: true,
+            staleTime: 0
+        },
+    },
+}
+
+export type ApiFnReturnType<FnType extends (...args: any) => Promise<any>> =
+  Awaited<ReturnType<FnType>>;
+
+export type QueryConfig<T extends (...args: any[]) => any> = Omit<
+  ReturnType<T>,
+  'queryKey' | 'queryFn'
+>;
+
+export type MutationConfig<
+  MutationFnType extends (...args: any) => Promise<any>,
+> = UseMutationOptions<
+  ApiFnReturnType<MutationFnType>,
+  Error,
+  Parameters<MutationFnType>[0]
+>;
+
+export const queryClient = new QueryClient(queryClientConfig);
---
+++ web/src/types/event.ts
@@ -0,0 +1,1 @@
+// TODO: Autogenerate types from OpenAPI spec
