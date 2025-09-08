227b199536cebefe7bfc7ac7d5060a635f023f1a
JeffreyJPZ <jeffreyjpizhang@gmail.com>
2025-09-07 17:54:29 -0700

    build: update linting rules

--- dashboard/next.config.ts
+++ dashboard/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  ignoreBuildErrors: true, // TODO: remove after fixing react-query typing
 };
 
 export default nextConfig;
