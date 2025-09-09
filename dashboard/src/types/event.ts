30a69551e9d200cfec257beccce275620b2c979c
JeffreyJPZ <jeffreyjpizhang@gmail.com>
2025-09-08 18:19:08 -0700

    feat(dashboard): make events before session start have negative elapsed time

--- dashboard/src/types/event.ts
+++ dashboard/src/types/event.ts
@@ -106,7 +106,7 @@ export const event = z
         category: eventCategory,
         cause: eventCause,
         date: z.iso.datetime({ offset: true }),
-        elapsed_time: z.string().regex(/^(\d{2}):(\d{2}):(\d{2})(?:\.(\d{1,6}))?$/),
+        elapsed_time: z.string().regex(/^-?(\d{2}):(\d{2}):(\d{2})(?:\.(\d{1,6}))?$/),
         details: eventDetails.nullish(),
         meeting_key: z.number().int().nonnegative(),
         session_key: z.number().int().nonnegative()
